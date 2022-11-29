2ddfdefd73fb792248a760cb6f0b695d6aabf78e
王凡 <wangfan@wangfandeimac.myfiosgateway.com>
2022-11-28 21:32:58 -0500

    a9-11-28

--- src/tuiter/tuits/tuits-schema.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-                                   tuit: String,
-                                   likes: Number,
-                                   liked: Boolean,
-                               }, {collection: 'tuits'});
-export default schema;
